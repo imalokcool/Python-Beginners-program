836dfd2c76a6b5878b4c60a4e57872b18f4b2337
imalokcool <mail2alok007@gmail.com>
2020-02-07 15:30:17 +0530

    updated

--- email_verification.py
+++ email_verification.py
@@ -4,6 +4,10 @@ if (email[0].isdigit()):
     print("Invalid mail")
 elif (email[0] == '@'):
     print("Invalid mail")
+elif (email.endswith('.' or email.endswith('@'))):
+    print("Invalid mail")
+elif(email.index('@') > email.index('.')):
+    print("Invalid mail")
 else:
     for i in range(0, len(email)):
         if (email[i] == ' '):
@@ -14,7 +18,7 @@ else:
                 atcount = atcount + 1
             if (email[i]=='.'):
                 dotcount = dotcount + 1
-    if (atcount>=1 and dotcount>=1):
+    if (atcount==1 and dotcount==1):
         print("Valid mail")
     else:
         print("Invalid mail")
